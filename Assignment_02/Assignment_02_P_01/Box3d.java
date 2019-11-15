bebe45b11d2a399b74dea9a465d4b43b91e09b66
Md.Mustafizur Rahman <33628705+MustafizurRU@users.noreply.github.com>
2019-11-16 01:13:07 +0600

    Update Box3d.java

--- Assignment_02/Assignment_02_P_01/Box3d.java
+++ Assignment_02/Assignment_02_P_01/Box3d.java
@@ -4,6 +4,12 @@ public class Box3d extends Box {
     Box3d(int length, int width, int height) {
         super(length, width, height);
     }
+    int area(){
+        return (length*width);
+    }
+    int volume(){
+        return (length*width*height);
+    }
 
 
 
