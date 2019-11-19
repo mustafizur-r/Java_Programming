edf5b505278a8f4674a9baf8e3997dc36e78f202
Md.Mustafizur Rahman <33628705+MustafizurRU@users.noreply.github.com>
2019-11-19 10:00:03 +0600

    Update NumOfOccur.java

--- Assignment_02/Assignment_02_P_03/NumOfOccur.java
+++ Assignment_02/Assignment_02_P_03/NumOfOccur.java
@@ -4,9 +4,11 @@ public class NumOfOccur {
     String st = "Information and Communication Engineering";
     int count = 0;
     int calNumOfOccur(Character  str){
-
+         str = Character.toUpperCase(str);
         for(int i = 0; i<st.length();i++){
+            
             Character ch = st.charAt(i);
+            ch = Character.toUpperCase(ch);
             if(str == ch){
                 count++;
             }
