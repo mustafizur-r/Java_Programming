dbbcb1cf38887270bfd9de3606d19fccbedbea3a
Md.Mustafizur Rahman <33628705+MustafizurRU@users.noreply.github.com>
2019-11-19 09:58:59 +0600

    Update Main.java

--- Assignment_02/Assignment_02_P_03/Main.java
+++ Assignment_02/Assignment_02_P_03/Main.java
@@ -1,8 +1,15 @@
 package Assignment_02_P_03;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         NumMain ob = new NumMain();
-        System.out.println("Number of Occurs is = "+ob.calNumOfOccur(ob.ch));
+        System.out.println(ob.st);
+        System.out.print("Enter The Character Which is Check = ");
+        ob.ch = input.next().charAt(0);
+        
+        System.out.printf("Number of %c Occurs =",ob.ch );
+        System.out.println(" "+ob.calNumOfOccur(ob.ch));
     }
 }
